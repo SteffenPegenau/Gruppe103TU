2221bf8cd102df485d807a27179aeeab0337f240
Steffen Pegenau <steffen.pegenau@gmail.com>
2015-03-18 10:00:38 +0100

    Korrektur des letzten Commits

--- src/de/tu_darmstadt/gdi1/gorillas/ui/states/GameplayState.java
+++ src/de/tu_darmstadt/gdi1/gorillas/ui/states/GameplayState.java
@@ -6,7 +6,9 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.GameState;
 import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.state.transition.Transition;
 
 import de.matthiasmann.twl.Button;
 import de.matthiasmann.twl.DialogLayout;
